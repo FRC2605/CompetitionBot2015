298040636dba3e920128cf0c1445b38df6c1cc48
Liam <liam.tab@gmail.com>
2015-02-04 17:00:36 -0800

    Initial commit
    Robot.cpp/h

---
+++ src/Robot.cpp
@@ -0,0 +1,18 @@
+#include "Robot.h"
+
+Robot :: Robot ()
+{
+	
+	
+	
+};
+
+Robot :: ~Robot ()
+{
+	
+	
+	
+};
+
+
+START_ROBOT_CLASS ( ROBOT_CLASS )
---
+++ src/Robot.h
@@ -0,0 +1,20 @@
+#ifndef SHS2605_ROBOT_H
+#define SHS2605_ROBOT_H
+
+#include <WPILib.h>
+
+class Robot : public IterativeRobot
+{
+public:
+	
+	Robot ();
+	~Robot ();
+	
+private:
+	
+	
+};
+
+#define ROBOT_CLASS Robot
+
+#endif
